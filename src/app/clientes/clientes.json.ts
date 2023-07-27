b889bf0344392da96159340d70b91acd64206b81
AntonioValentin <71309949+AntonioValentinSayago@users.noreply.github.com>
2023-07-26 20:31:29 -0600

    Delete src directory

--- src/app/clientes/clientes.json.ts
+++
@@ -1,8 +0,0 @@
-import { Cliente } from './cliente';
-
-export const CLIENTES: Cliente[] =
-[
-  {id: 1, nombre: 'Antonio', apellido:'Valentin', email:'antoniovalentin@gmail.com',createAt:'2023-04-25'},
-  {id: 2, nombre: 'Marcos', apellido:'Eduardo', email:'marcoseduardo@gmail.com',createAt:'2023-04-25'},
-  {id: 3, nombre: 'Montse', apellido:'Benicio', email:'montsebenicio@gmail.com',createAt:'2023-04-25'},
-];
